f2b849da7130b6962878e312cb052cbd813cf688
Jiwoong Jung <kanziwoong@gmail.com>
2023-02-18 17:14:37 +0900

    Add slack webhook skel

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,12 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { type SlackEvent } from './slack'
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
+  SLACK_SIGNING_SECRET: string
+  SLACK_BOT_TOKEN: string
+  SLACK_APP_TOKEN: string
 }
+
+const BOT_NAME = 'Deploy 🤖'
 
 export default {
   async fetch (
@@ -29,6 +14,24 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    return new Response('Hello World!')
+    const body = await request.json<SlackEvent>()
+    console.log(' BODY', JSON.stringify(body, null, 2))
+
+    switch (body.type) {
+      case 'url_verification':
+        return json(body)
+      case 'event_callback':
+          // TODO: Handle event
+    }
+
+    return new Response('success')
   }
+}
+
+function json (obj: object): Response {
+  return new Response(JSON.stringify(obj), {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
 }
---
+++ src/slack.ts
@@ -0,0 +1,102 @@
+interface BaseEvent { type: 'url_verification' | 'event_callback' }
+
+/**
+ * Sample Value
+{
+  token: 'xqDNBFJKHK3I7g68iTqjPmZV',
+  challenge: '8nbLGUs5Biu0vjvxBOmFdUiFxxJlyxiGFahI7EbERhuxqVb3zqNJ',
+  type: 'url_verification'
+}
+ */
+interface UrlVerificationEvent extends BaseEvent {
+  token: string
+  challenge: string
+  type: 'url_verification'
+}
+
+/**
+ * Sample Value
+{
+  "token": "xqDNBFJKHK3I7g68iTqjPmZV",
+  "team_id": "T0C946V4G",
+  "api_app_id": "A02R9DV92EQ",
+  "event": {
+    "client_msg_id": "4d6237ee-8257-499a-9e5b-a675e563d6b7",
+    "type": "app_mention",
+    "text": "<@U02RWTNJFPS> hi",
+    "user": "U3X879SH2",
+    "ts": "1676707824.766549",
+    "blocks": [
+      {
+        "type": "rich_text",
+        "block_id": "Cjf0t",
+        "elements": [
+          {
+            "type": "rich_text_section",
+            "elements": [
+              {
+                "type": "user",
+                "user_id": "U02RWTNJFPS"
+              },
+              {
+                "type": "text",
+                "text": " hi"
+              }
+            ]
+          }
+        ]
+      }
+    ],
+    "team": "T0C946V4G",
+    "channel": "C0C92MJFQ",
+    "event_ts": "1676707824.766549"
+  },
+  "type": "event_callback",
+  "event_id": "Ev04Q20A9PJA",
+  "event_time": 1676707824,
+  "authorizations": [
+    {
+      "enterprise_id": null,
+      "team_id": "T0C946V4G",
+      "user_id": "U02RWTNJFPS",
+      "is_bot": true,
+      "is_enterprise_install": false
+    }
+  ],
+  "is_ext_shared_channel": false,
+  "event_context": "4-eyJldCI6ImFwcF9tZW50aW9uIiwidGlkIjoiVDBDOTQ2VjRHIiwiYWlkIjoiQTAyUjlEVjkyRVEiLCJjaWQiOiJDMEM5Mk1KRlEifQ"
+}
+ */
+interface EventCallbackEvent extends BaseEvent {
+  token: string
+  team_id: string
+  api_app_id: string
+  event: {
+    client_msg_id: string
+    type: 'app_mention'
+    text: string
+    user: string
+    ts: string
+    blocks: Block[]
+    team: string
+    channel: string
+    event_ts: string
+  }
+  type: 'event_callback'
+  event_id: string
+  event_time: number
+  authorizations: Authentication[]
+  is_ext_shared_channel: boolean
+  event_context: string
+}
+
+type Block = unknown
+interface Authentication {
+  enterprise_id: null
+  team_id: string
+  user_id: string
+  is_bot: boolean
+  is_enterprise_install: boolean
+}
+
+export type SlackEvent = UrlVerificationEvent | EventCallbackEvent
