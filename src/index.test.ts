33431cadb1a8cee52ee521197fb2ea268aeda7d1
Jiwoong Jung <kanziwoong@gmail.com>
2023-02-18 19:45:16 +0900

    Add EventCallbackEvent test but skip

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,12 +3,18 @@ import type { UnstableDevWorker } from 'wrangler'
 import { describe, expect, it, beforeAll, afterAll } from 'vitest'
 import { type SlackEvent } from './slack'
 
+type DeepPartial<T> = T extends object ? {
+  [P in keyof T]?: DeepPartial<T[P]>;
+} : T
+
 describe('Worker', () => {
   let worker: UnstableDevWorker
+  const fakeBotId = 'FAKE_BOT_ID'
 
   beforeAll(async () => {
     worker = await unstable_dev('src/index.ts', {
       experimental: { disableExperimentalWarning: true },
+      vars: { BOT_ID: fakeBotId },
     })
   })
 
@@ -17,7 +23,7 @@ describe('Worker', () => {
   })
 
   describe('UrlVerificationEvent', () => {
-    it('should handle url_verification event', async () => {
+    it('success', async () => {
       const urlVerificationEvent: SlackEvent = {
         token: 'xqDNBFJKHK3I7g68iTqjPmZV',
         challenge: '8nbLGUs5Biu0vjvxBOmFdUiFxxJlyxiGFahI7EbERhuxqVb3zqNJ',
@@ -31,6 +37,28 @@ describe('Worker', () => {
 
       const json = await resp.json()
       expect(json).toEqual(urlVerificationEvent)
+    })
+  })
+
+  // There is no idea to mock slackClient
+  describe.skip('EventCallbackEvent', () => {
+    it('success', async () => {
+      const eventCallbackEvent: DeepPartial<SlackEvent> = {
+        event: {
+          type: 'app_mention',
+          text: `<@${fakeBotId}> echo hi`,
+          channel: 'C0C92MJFQ',
+        },
+        type: 'event_callback',
+      }
+
+      const resp = await worker.fetch('', {
+        method: 'POST',
+        body: JSON.stringify(eventCallbackEvent),
+      })
+
+      const text = await resp.text()
+      expect(text).toEqual('success')
     })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import { type AppMentionEvent, type SlackEvent } from './slack'
 import { slackClient, type SlackClient } from './slackClient'
 
 export interface Env {
-  SLACK_SIGNING_SECRET: string
-  SLACK_BOT_TOKEN: string
-  SLACK_APP_TOKEN: string
-}
+  // Vars
+  BOT_ID: string
 
-const BOT_ID = 'U02RWTNJFPS'
+  // Secrets
+  SLACK_BOT_TOKEN: string
+}
 
 const helpMessage = `I have the following features
 
@@ -15,7 +15,11 @@ const helpMessage = `I have the following features
 > Echoes the message back to the channel
 `
 
-let slackCli: SlackClient
+interface Config {
+  botId: string
+  slackCli: SlackClient
+}
+let cfg: Config
 
 export default {
   async fetch (
@@ -23,15 +27,22 @@ export default {
     env: Env,
     _ctx: ExecutionContext,
   ): Promise<Response> {
-    if (!slackCli) {
-      slackCli = slackClient({ botToken: env.SLACK_BOT_TOKEN })
+    if (!cfg) {
+      cfg = {
+        botId: env.BOT_ID,
+        slackCli: slackClient({ botToken: env.SLACK_BOT_TOKEN }),
+      }
     }
 
     const body = await request.json<SlackEvent>()
     console.log(' [DEBUG] BODY', JSON.stringify(body, null, 2))
 
     if (body.type === 'url_verification') {
-      return json(body)
+      return new Response(JSON.stringify(body), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
     }
 
     if (body.type !== 'event_callback' || !body.event) {
@@ -39,15 +50,15 @@ export default {
     }
 
     switch (body.event.type) {
-      case 'app_mention': await handleAppMentionEvent(slackCli, body.event); break
+      case 'app_mention': await handleAppMentionEvent(cfg, body.event); break
     }
 
     return new Response('success')
   },
 }
 
-async function handleAppMentionEvent (slackCli: SlackClient, event: AppMentionEvent): Promise<void> {
-  const [command, ...args] = event.text.trim().replace(`<@${BOT_ID}> `, '').split(' ')
+async function handleAppMentionEvent ({ botId, slackCli }: Config, event: AppMentionEvent): Promise<void> {
+  const [command, ...args] = event.text.trim().replace(`<@${botId}> `, '').split(' ')
   console.log(' [DEBUG] COMMAND', { command, args: JSON.stringify(args) })
 
   switch (command) {
@@ -59,12 +70,4 @@ async function handleAppMentionEvent (slackCli: SlackClient, event: AppMentionEv
     default:
       await slackCli.postMessage(event.channel, helpMessage)
   }
-}
-
-function json (obj: object): Response {
-  return new Response(JSON.stringify(obj), {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
 }
